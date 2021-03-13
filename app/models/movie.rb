766bf593746b34af3fe64ca882edb55f09c42b58
joesuffredini <gsuffred@gmail.com>
2021-03-13 16:20:44 -0500

    adds genre model, musicGenre model, and json vieew update

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,16 @@ class Movie < ApplicationRecord
   validates :plot, presence: true
   validates :director, length: { minimum: 100 }
   validates :english, confirmation: true
-  has_many  :actors  
+  has_many  :actors
+  has_many  :movie_genres
+  has_many  :genres, through: :movie_genres
+
+
+def genre_names
+  movie_genres.map do |movie_genre|
+    movie_genre.genre[:name]
+  end
+end
+
+
 end
